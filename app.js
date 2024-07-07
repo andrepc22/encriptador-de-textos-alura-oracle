40d1fed85ed6ed3f61f144e03b22099f74d60859
André Pasache <wandrepc15@gmail.com>
2024-07-07 02:11:39 -0500

    lógica encriptar - desencriptar

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
-const textToDo = document.getElementsByTagName("input-text")[0];
+function encriptarTexto() {
+  const texto = document.getElementById("inputTexto").value;
+  const textoEncrip = encriptar(texto);
+  //   console.log(textoEncrip);
+  document.getElementById("outputTexto").value = textoEncrip;
+}
 
-console.log(textToDo);
+function encriptar(texto) {
+  let resultado = "";
+  for (let i = 0; i < texto.length; i++) {
+    let newnew = "";
+    switch (texto[i]) {
+      case "a":
+        newnew = "ai";
+        break;
+      case "e":
+        newnew = "enter";
+        break;
+      case "i":
+        newnew = "imes";
+        break;
+      case "o":
+        newnew = "ober";
+        break;
+      case "u":
+        newnew = "ufat";
+        break;
+      default:
+        newnew = texto[i];
+    }
+    resultado += newnew;
+  }
+  return resultado;
+}
+
+function desencriptarTexto() {
+  const texto = document.getElementById("inputTexto").value;
+  const textoEncrip = desencriptar(texto);
+  //   console.log(textoEncrip);
+  document.getElementById("outputTexto").value = textoEncrip;
+}
+
+function desencriptar(texto) {
+  let resultado = texto;
+  const llaves = ["ai", "enter", "imes", "ober", "ufat"];
+  const vocales = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < 5; i++) {
+    resultado = resultado.replaceAll(llaves[i], vocales[i]);
+    // console.log(resultado);
+  }
+
+  return resultado;
+}
